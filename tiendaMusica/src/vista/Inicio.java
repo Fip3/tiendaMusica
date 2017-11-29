e82e8cacabe1c257ebf996620ae41284da72b47e
Felipe Torrejon <felipetorrejon@live.cl>
2017-11-29 11:27:51 -0300

    vista - inicio - menu y funcionalidad

--- tiendaMusica/src/vista/Inicio.java
+++ tiendaMusica/src/vista/Inicio.java
@@ -5,6 +5,8 @@
  */
 package vista;
 
+import modelo.*;
+import controlador.*;
 /**
  *
  * @author felip
@@ -16,6 +18,8 @@ public class Inicio extends javax.swing.JFrame {
      */
     public Inicio() {
         initComponents();
+        RegistroInstrumento inventario = new RegistroInstrumento();
+        
     }
 
     /**
@@ -27,10 +31,65 @@ public class Inicio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuBarInicio = new javax.swing.JMenuBar();
+        jMenuArchivo = new javax.swing.JMenu();
+        jMenuSalir = new javax.swing.JMenuItem();
+        jMenuOpciones = new javax.swing.JMenu();
+        jMenuAgregar = new javax.swing.JMenuItem();
+        jMenuEliminar = new javax.swing.JMenuItem();
+        jMenuListar = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Inicio");
         setName("jFrameInicio"); // NOI18N
         setResizable(false);
+
+        jMenuArchivo.setText("Archivo");
+
+        jMenuSalir.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_X, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuSalir.setText("Salir");
+        jMenuSalir.setToolTipText("");
+        jMenuSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuSalirActionPerformed(evt);
+            }
+        });
+        jMenuArchivo.add(jMenuSalir);
+
+        jMenuBarInicio.add(jMenuArchivo);
+
+        jMenuOpciones.setText("Opciones");
+
+        jMenuAgregar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_A, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuAgregar.setText("Agregar instrumento...");
+        jMenuAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuAgregarActionPerformed(evt);
+            }
+        });
+        jMenuOpciones.add(jMenuAgregar);
+
+        jMenuEliminar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_E, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuEliminar.setText("Eliminar instrumento...");
+        jMenuEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuEliminarActionPerformed(evt);
+            }
+        });
+        jMenuOpciones.add(jMenuEliminar);
+
+        jMenuListar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_L, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuListar.setText("Listar instrumentos");
+        jMenuListar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuListarActionPerformed(evt);
+            }
+        });
+        jMenuOpciones.add(jMenuListar);
+
+        jMenuBarInicio.add(jMenuOpciones);
+
+        setJMenuBar(jMenuBarInicio);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -45,6 +104,26 @@ public class Inicio extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuAgregarActionPerformed
+        Agregar agregar = new Agregar();
+        agregar.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jMenuAgregarActionPerformed
+
+    private void jMenuSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuSalirActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jMenuSalirActionPerformed
+
+    private void jMenuEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuEliminarActionPerformed
+        Eliminar eliminar = new Eliminar();
+        eliminar.setVisible(true);
+    }//GEN-LAST:event_jMenuEliminarActionPerformed
+
+    private void jMenuListarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuListarActionPerformed
+        Listar listar = new Listar();
+        listar.setVisible(true);
+    }//GEN-LAST:event_jMenuListarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -82,5 +161,12 @@ public class Inicio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem jMenuAgregar;
+    private javax.swing.JMenu jMenuArchivo;
+    private javax.swing.JMenuBar jMenuBarInicio;
+    private javax.swing.JMenuItem jMenuEliminar;
+    private javax.swing.JMenuItem jMenuListar;
+    private javax.swing.JMenu jMenuOpciones;
+    private javax.swing.JMenuItem jMenuSalir;
     // End of variables declaration//GEN-END:variables
 }
